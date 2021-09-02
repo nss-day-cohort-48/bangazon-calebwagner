5bccead04cb6346ef239f3f40fa38c1e6fa3e331
Caleb Wagner <calebwagner5@gmail.com>
2021-09-02 10:15:00 -0500

    added url path for view that handles logic for incomplete orders report

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import user_favorite_list
+from .views import user_favorite_list, incomplete_orders_list
 
 urlpatterns = [
     path('reports/userfavorites', user_favorite_list),
+    path('reports/incompleteorders', incomplete_orders_list),
 ]
